8b4506c198d80e92d368386d3aec2b8682632a4e
ValCMoi <valentin.rouxel.pro@gmail.com>
2023-04-16 12:51:16 +0200

    ajout class pokemon

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './class/pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+
+export class AppComponent implements OnInit{
+  private pokemons: Pokemon[];
+
+  constructor(){}
+
+  ngOnInit(): void {
+      this.pokemons = []
+  }
 }
